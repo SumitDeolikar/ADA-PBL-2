7a781b476860917b12c4d5651439fb65362633d2
Sumit Deolikar <147622644+SumitDeolikar@users.noreply.github.com>
2023-10-23 01:28:42 +0530

    Update adapbl1.cpp

--- adapbl1.cpp
+++ adapbl1.cpp
@@ -3,20 +3,7 @@
 using namespace std;
 
 bool isPossible(vector<int> arr, int n, int m, int mid) {
-    int studentCount = 1;
-    int pageSum = 0;
     
-    for(int i = 0; i < n; i++) {
-        if(pageSum + arr[i] <= mid) {
-            pageSum += arr[i];
-        }
-        else {
-            studentCount++;
-            if(studentCount > m || arr[i] > mid) {
-                return false;
-            }
-            pageSum = arr[i];
-        }
 }
 
 int allocateBooks(vector<int> arr, int n, int m) {
